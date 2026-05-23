9ab5bfda47358e6ef1eb5a8337b6d3fa545e3ebd
saiabhiramyelloju <abhi.yelloju@gmail.com>
2026-05-23 10:10:08 +0530

    given js logic

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-let task = "todo"
+const form = document.getElementById("todo-form");
+const inout = document.getElementById("todo-input");
+const todolist = document.getElementById("todo-list");
+
+form.addEventListener("submit",function(event)
+{
+    event.preventDefault();
+    const task = InputDeviceInfo.value;
+    const li = document.createElement("li");
+    li.innertext = taskText;
+    todolist.appendChild(li);
+    InputDeviceInfo.value = "";
+});
